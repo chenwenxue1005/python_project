ab42616e6aadcc6a84999e2e8014579d90dbcfb0
xing62943 <14928102+xing62943@user.noreply.gitee.com>
2024-09-23 01:07:19 +0800

    版本回退后的又一次修改

--- python.py
+++ python.py
@@ -6,7 +6,7 @@ known_image = face_recognition.load_image_file("known_person.jpg")
 known_encoding = face_recognition.face_encodings(known_image)[0]
 
 # 初始化视频捕捉
-video_capture = cv2.VideoCapture(1)
+video_capture = cv2.VideoCapture(0)
 
 while True:
     # 捕捉视频中的每一帧
@@ -43,3 +43,4 @@ while True:
 # 释放摄像头和窗口
 video_capture.release()
 cv2.destroyAllWindows()
+cv2.waitKey()
